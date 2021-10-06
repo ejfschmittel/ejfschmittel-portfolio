4615fea6c20d5d8bf4e886c915ae05d7a98b76b5
ejfschmittel <ejfschmittel@aol.de>
2021-10-06 17:16:26 +0200

    added basic mobile menu

--- src/components/Header.component.jsx
+++ src/components/Header.component.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {smoothScrollTo} from "../utils/utils"
 import "../styles/components/header.styles.scss";
 import "../styles/components/logo.styles.scss";
 
 const Header = () => {
 
+    const [navOpen, setNavOpen] = useState(false);
+    
 
-    const onProjectsClick = () => smoothScrollTo("section-projects")
-    const onContactClick = () => smoothScrollTo("section-contact")
-    const onHomeClick = () => smoothScrollTo("section-hero")
+    const toggleNav = () => {
+        setNavOpen(!navOpen)
+    }
+
+    const onProjectsClick = () => { setNavOpen(false); smoothScrollTo("section-projects");}
+    const onContactClick = () => { setNavOpen(false); smoothScrollTo("section-contact");}
+    const onHomeClick = () => { setNavOpen(false); smoothScrollTo("section-hero");}
 
     return (
         <header className="header">
@@ -16,13 +22,28 @@ const Header = () => {
                 <Logo />
             </div>
 
-            <nav className="header__nav">
-                <ul>
+            <nav className={`header__nav ${navOpen ? "header__nav--open" : ""}`}>
+
+                <button className="header__menu-btn" onClick={toggleNav}>
+
+                    <div className="header__btn-bars">
+                    <div className="header__btn-bar header__btn-bar--1"></div>
+                    <div className="header__btn-bar header__btn-bar--2"></div>
+                    <div className="header__btn-bar header__btn-bar--3"></div>
+                    </div>
+                </button>
+
+              
+
+
+                <ul className="header__nav-links">
                     <li className="header__nav-item"><a onClick={onHomeClick}>Home</a></li>
                     <li className="header__nav-item"><a onClick={onProjectsClick}>Projects</a></li>
                     <li className="header__nav-item"><a onClick={onContactClick}>Contact Me</a></li>
                 </ul>
             </nav>
+
+           
         </header>
     )
 }
